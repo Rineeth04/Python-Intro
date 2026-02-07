90a856cd089056cbef19eb09a08437e2ed80d1e1
Rineeth <venepallyrineeth@gmail.com>
2026-02-07 15:50:59 +0530

    Added new folder with examples

--- Basic programs/example1.py
+++
@@ -1,3 +0,0 @@
-print("hello world")
-name = "John"
-print("Hello, " + name + "!")  # Concatenating strings
---
+++ Exception Handling/Example1.py
@@ -0,0 +1,11 @@
+# catching Specific Exceptions
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    result = num1 / num2
+    print("Result:", result)
+except ZeroDivisionError:
+    print("Error: Cannot divide by zero.")
+except ValueError:
+    print("Error: Invalid input. Please enter a valid integer.")
+    
